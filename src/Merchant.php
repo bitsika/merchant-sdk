7a5fff1cbc1b962840bcd3bc9defb5d7039b9be7
ajibukunoluwa <ibukunajmoti@gmail.com>
2022-04-11 19:08:18 +0100

    added customer service

--- src/Merchant.php
+++ src/Merchant.php
@@ -4,11 +4,19 @@ namespace Bitsika;
 
 use Bitsika\Resources\Supports\Http;
 use Bitsika\Resources\Services\Invoice;
+use Bitsika\Resources\Services\Customer;
 use Bitsika\Resources\Services\Transaction;
 use Bitsika\Resources\Config\MerchantConfig;
 
 class Merchant
 {
+    /**
+     * Merchant test secret key prefix
+     * 
+     * @var string
+     */
+    const TEST_KEY_PREFIX = 'bsk_sec_test_';
+    
     /**
      * Merchant Secret Key 
      * 
@@ -49,17 +57,10 @@ class Merchant
         if (! is_string($secretKey) || ! (substr($secretKey, 0, 8) === 'bsk_sec_')) {
             throw new \InvalidArgumentException("Invalid secret key passed. {$secretKey}");
         }
-        
-        if (! is_string($secretKey) || ! (substr($secretKey, 0, 8) === 'bsk_sec_')) {
-            throw new \InvalidArgumentException("Invalid secret key passed. {$secretKey}");
-        }
 
-        $this->isTestEnvironment = is_string($secretKey) && (substr($secretKey, 0, 13) === 'bsk_sec_test_');
-        
-        $this->http = new Http($this->isTestEnvironment ? MerchantConfig::STAGING_API_BASE_URL : MerchantConfig::API_BASE_URL);
-
-        $this->http->withToken($secretKey)
-            ->withHeader(['x-service-name' => MerchantConfig::SERVICE_NAME]);
+        $this->secretKey = $secretKey;
+        $this->setEnvironment();
+        $this->setHttpService();
     }
 
     /**
@@ -106,5 +107,42 @@ class Merchant
     public function transaction(): Transaction
     {
         return new Transaction($this->http);
+    }
+
+    /**
+     * Get an instance of the customer service
+     * 
+     * @return Customer
+     */
+    public function customer(): Customer
+    {
+        return new Customer($this->http);
+    }
+
+    /**
+     * Set the request environment
+     * 
+     * @return void
+     */
+    private function setEnvironment(): void
+    {
+        $this->isTestEnvironment = is_string($this->secretKey) && (substr($this->secretKey, 0, 13) === 'bsk_sec_test_');
+    }
+
+    /**
+     * Set the request Http service
+     * 
+     * @return void
+     */
+    private function setHttpService(): void
+    {
+        $this->http = new Http(
+            $this->isTestEnvironment ? 
+            MerchantConfig::STAGING_API_BASE_URL : 
+            MerchantConfig::API_BASE_URL
+        );
+
+        $this->http->withToken($this->secretKey)
+            ->withHeader(['x-service-name' => MerchantConfig::SERVICE_NAME]);
     }
 }
